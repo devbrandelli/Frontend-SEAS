a44642819eadc5628c5ffc8bfe254f1e2a96dca1
devbrandelli <febrandelli.dev@gmail.com>
2020-04-25 01:09:18 -0300

    implementado use history

--- src/Pages/Logon/index.js
+++ src/Pages/Logon/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import logoImg from '../../Assets/logo.png';
 import image from '../../Assets/imageLogon.png'
 
 import './style.css';
 
+
 export default function Logon() {
+  const history = useHistory();
+
+  function handleLogin(e){
+    e.preventDefault();
+
+    history.push('/main')
+    
+  }
+  
   return (
     <div className="logon-container">
     <section className="form">
       <img src={logoImg} alt="Be the hero" />
 
-      <form >
+      <form onSubmit={handleLogin} >
         <h1>Faça seu logon</h1>
         
         <input 
