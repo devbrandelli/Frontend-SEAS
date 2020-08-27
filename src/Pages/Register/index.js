52477c05fbe67c40e9b8250aa851c27bba4c4257
felipe <fe.brandelli@gmail.com>
2020-08-26 21:31:35 -0300

    Atualizado pagina de cadastro 26-08

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -3,19 +3,24 @@ import Header from '../../Utils/Header'
 import './style.css';
 
 function Register() {
-    
+
     return <div>
         <Header />
         <form>
             <div>Pagina de cadastro</div>
-            <label>Data:</label>
+            <label>Data:
             <input type="date" />
+            </label>
+            <label id="hora">Hora:
+            <input type="time" />
+            </label><br/>
             <label>Local da abordagem: </label>
             <input type="text" />
             <label>Motivo da abordagem: </label>
             <input type="text" />
-            <label>Data de nascimento: </label>
+            <label> Data de nascimento: 
             <input type="date" />
+            </label><br/>
             <label className="mainLabel">Gênero: </label>
             <div id="sexoInput">
                 <div className="radioLine">
@@ -86,13 +91,65 @@ function Register() {
                 </div>
             </div>
             <label>Tempo em situação de rua: </label>
-            <input type="text" />
+            <div id="tempoRuaInput">
+                <div className="radioLine">
+                    <input type="radio" id="ate6m" name="tempoRua" value="ate6m" className="radioInput" />
+                    <label>Ate 6 meses</label>
+                </div>
+                <div className="radioLine">
+                    <input type="radio" id="6mA1a" name="tempoRua" value="6mA1a" className="radioInput" />
+                    <label>De 6 meses a 1 ano</label>
+                </div>
+                <div className="radioLine">
+                    <input type="radio" id="1aA2a" name="tempoRua" value="1aA2a" className="radioInput" />
+                    <label>De 1 ano a 2 anos</label>
+                </div>
+                <div className="radioLine">
+                    <input type="radio" id="2aOU+" name="tempoRua" value="2aOU+" className="radioInput" />
+                    <label>2 anos ou mais</label>
+                </div>
+            </div>
             <label>Motivo que levou para a rua: </label>
-            <input type="text" />
+            <div id="motivoRuaInput">
+                <div className="radioLine">
+                    <input type="checkbox" id="alcool" name="motivo" value="alcool" className="radioInput" />
+                    <label>Alcool</label>
+                </div>
+                <div className="radioLine">
+                    <input type="checkbox" id="drogas" name="motivo" value="drogas" className="radioInput" />
+                    <label>Multiplas Drogas</label>
+                </div>
+                <div className="radioLine">
+                    <input type="checkbox" id="familiar" name="motivo" value="familiar" className="radioInput" />
+                    <label>Conflito Familiar</label>
+                </div>
+                <div className="radioLine">
+                    <input type="checkbox" id="renda" name="motivo" value="renda" className="radioInput" />
+                    <label>Ausencia de renda</label>
+                </div>
+                <div className="radioLine">
+                    <input type="checkbox" id="prisao" name="motivo" value="prisao" className="radioInput" />
+                    <label>Regresso prisional</label>
+                </div>
+                <div className="radioLine">
+                    <input type="checkbox" id="outros" name="motivo" value="outros" className="radioInput" />
+                    <label>Outros</label>
+                </div>
+                
+            </div>
             <label>Local de Origem: </label>
             <input type="text" />
             <label>Tem interesse em sair da rua: </label>
-            <input type="text" />
+            <div id="sairRuaInput">
+                <div className="radioLine">
+                    <input type="radio" id="sim" name="sairRua" value="sim" className="radioInput" />
+                    <label>Sim</label>
+                </div>
+                <div className="radioLine">
+                    <input type="radio" id="não" name="sairRua" value="não" className="radioInput" />
+                    <label>Não</label>
+                </div>
+            </div>
             <label>O que precisa para sair da rua: </label>
             <input type="text" />
             <label>Qual serviço busca: </label>
